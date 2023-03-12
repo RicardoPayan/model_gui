57462afa61f9fb36fe8a4900fff04e6d1a3dd94c
RicardoPayan <a218206581@unison.mx>
2023-03-12 12:02:34 -0700

    ya corre el model en la interfaz, pero falta arreglar detalles, como que se impriman las predicciones

--- prediction_gui.py
+++ prediction_gui.py
@@ -4,12 +4,129 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+from collections import deque
 
 
-predicciones = ['step-1','step-2','step-3'] #Solo para probar como imprimir las predicciones
+
+
+
+def select_model(src):
+    try:
+        model = load_model(src)
+    except (OSError):
+        print(f"[ERROR] No model found at {src}")
+        return None
+    return model
+
+#Seccion para abrir la camara sin predicir todavia.
+def open_camera():
+    _,frame = capture.read() #Leyendo los frames de la camara
+
+    frame = cv2.flip(frame,1)
+    #Convertir imagen de la camara
+    opencv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+
+    #Transformar el ultimo frame en una imagen
+    captured_image = Image.fromarray(opencv_image)
+
+    #Convertir imagen capturada en una photoimage para poder usar en tkinter
+    photo_image = ImageTk.PhotoImage(image=captured_image)
+
+    #Poner photo image en label de la camara.
+    label_camara.photo_image=photo_image
+    label_camara.configure(image=photo_image)
+
+    #Actualizar cada 10ms
+    label_camara.after(10,open_camera)
+
+def real_time(log=lambda *_ : None):
+    CLASSES_LIST = ["step-1", "step-2", "step-3", "step-4", "finished", "idle"]
+    IMAGE_HEIGHT, IMAGE_WIDTH = 64, 64
+    SEQUENCE_LENGTH = 30
+    MIN_PROB = 0.8
+
+    model = select_model('v2\idles_model2.h5')
+    
+    if model is None:
+        exit(0)
+
+    FPS = int(capture.get(cv2.CAP_PROP_FPS))
+    frames = 0
+    counter = 0
+    frames_queue = deque(maxlen=SEQUENCE_LENGTH)
+    current_step = ""
+    total_time = 0.0
+
+    while True:
+
+        ok, frame = capture.read()
+        frames += 1
+        counter += 1
+        if not ok:
+            break
+
+        frame = cv2.flip(frame, 1)
+
+            
+        opencv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+        captured_image = Image.fromarray(opencv_image)
+        photo_image = ImageTk.PhotoImage(image=captured_image)
+        label_camara.photo_image=photo_image
+        label_camara.configure(image=photo_image)
+        label_camara.after(10,open_camera)
+        
+        #Procesar data para el modelo
+        resized_frame = cv2.resize(frame, (IMAGE_WIDTH, IMAGE_HEIGHT))
+        normalized_frame = resized_frame / 255
+
+        frames_queue.append(normalized_frame)
+
+        if counter == SEQUENCE_LENGTH:
+            counter = 0
+
+            predicted_labels_probs = model.predict(
+                np.expand_dims(frames_queue, axis=0), verbose=0)[0]
+            predicted_label = np.argmax(predicted_labels_probs)
+            label_probability = predicted_labels_probs[predicted_label]
+            print(np.expand_dims(frames_queue, axis=0).shape)
+
+            # si no tengo confianza en la prediccion de un step marco (NO DETECTADO)
+            if label_probability < MIN_PROB:
+                current_step = ""
+            else:
+                if (current_step != CLASSES_LIST[predicted_label]):
+                    current_step = CLASSES_LIST[predicted_label]
+                    time_taken = frames / FPS
+                    total_time += time_taken
+                    frames = 0
+
+                    log(current_step, time_taken, total_time)
+        
+        i=0
+        if current_step:
+            print(current_step)
+            resultado = ttk.Label(prediction_frame,text=current_step)
+            resultado.grid(column=0,row=i)
+            i+=1
+        else:
+            print('no step detected')
+            resultado = ttk.Label(prediction_frame,text="no step detected")
+            resultado.grid(column=0,row=i)
+            i+=1
+
+        if counter > 60:
+            break
+
+    print('prediciendo')
 
 #Abrir la camara con opencv
-capture = cv2.VideoCapture(0)
+try:
+    capture = cv2.VideoCapture(0)
+except (IOError):
+        print(f"[ERROR] Can't open webcam.")
+
 width_camera, height_camera = 600,450
 
 
@@ -32,7 +149,7 @@ prediction_frame = Frame(content, width=650, height=400,borderwidth=5,relief='ri
 
 #BOTONES
 #Area de la camara
-start_prediction = ttk.Button(content, text='Iniciar')
+start_prediction = ttk.Button(content, text='Iniciar', command=real_time)
 stop_prediction = ttk.Button(content, text='Detener')
 
 #Area de predicciones
@@ -75,32 +192,11 @@ content.columnconfigure(3,weight=1)
 content.columnconfigure(4,weight=1)
 content.rowconfigure(1,weight=1)
 
-#Seccion para abrir la camara sin predicir todavia.
-def open_camera():
-    _,frame = capture.read() #Leyendo los frames de la camara
 
-    #Convertir imagen de la camara
-    opencv_image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
-
-    #Transformar el ultimo frame en una imagen
-    captured_image = Image.fromarray(opencv_image)
-
-    #Convertir imagen capturada en una photoimage para poder usar en tkinter
-    photo_image = ImageTk.PhotoImage(image=captured_image)
-
-    #Poner photo image en label de la camara.
-    label_camara.photo_image=photo_image
-    label_camara.configure(image=photo_image)
-
-    #Actualizar cada 10ms
-    label_camara.after(10,open_camera)
 
 open_camera()
 
-#Este for es solo de prueba para tener una idea de como imprimir las predicciones cuando se hagan en tiempo real
-for i in range(len(predicciones)):
-    resultado = ttk.Label(prediction_frame,text=predicciones[i])
-    resultado.grid(column=0,row=i)
+
 
 
 root.mainloop() #Correr la interfaz.
